8e77522828a5c8c7e9aede2654cc58c26077129e
Richard Rosato <rosatorichard@gmail.com>
2017-08-04 14:37:31 -0400

    permission to use GPS

--- app/src/main/java/posidenpalace/com/sirichan/view/activities/gps/GPS.java
+++ app/src/main/java/posidenpalace/com/sirichan/view/activities/gps/GPS.java
@@ -1,6 +1,9 @@
 package posidenpalace.com.sirichan.view.activities.gps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,8 +18,9 @@ import javax.inject.Inject;
 import posidenpalace.com.sirichan.R;
 import posidenpalace.com.sirichan.view.injection.gps.DaggerGPSComponent;
 
-public class GPS extends FragmentActivity implements OnMapReadyCallback,GPSContract.View {
-    @Inject GPSPresenter presenter;
+public class GPS extends FragmentActivity implements OnMapReadyCallback, GPSContract.View {
+    @Inject
+    GPSPresenter presenter;
     private GoogleMap mMap;
 
     @Override
@@ -31,7 +35,7 @@ public class GPS extends FragmentActivity implements OnMapReadyCallback,GPSContr
         presenter.addView(this);
     }
 
-    public void setupDagger(){
+    public void setupDagger() {
         DaggerGPSComponent.create().inject(this);
     }
 
@@ -39,6 +43,18 @@ public class GPS extends FragmentActivity implements OnMapReadyCallback,GPSContr
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(33, -87);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
--- app/src/main/java/posidenpalace/com/sirichan/view/activities/main_menu/MainMenu.java
+++ app/src/main/java/posidenpalace/com/sirichan/view/activities/main_menu/MainMenu.java
@@ -1,7 +1,14 @@
 package posidenpalace.com.sirichan.view.activities.main_menu;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import javax.inject.Inject;
 
@@ -9,6 +16,8 @@ import posidenpalace.com.sirichan.R;
 import posidenpalace.com.sirichan.view.injection.main_menu.DaggerMainMenuComponent;
 
 public class MainMenu extends AppCompatActivity implements MainMenuContract.View {
+    private static final int MY_PERMISSIONS_REQUEST_REQUEST_LOCATION = 0;
+    private static final String TAG = "MainMenu";
     @Inject MainMenuPresenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,63 @@ public class MainMenu extends AppCompatActivity implements MainMenuContract.View
         setContentView(R.layout.activity_main_menu);
         setupDagger();
         presenter.addView(this);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onCreate: Permission not granted");
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Log.d(TAG, "onCreate: We need this");
+
+                Toast.makeText(this, "App can not Continue Location Tracking is off", Toast.LENGTH_LONG).show();
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_REQUEST_LOCATION);
+                Log.d(TAG, "onCreate: Requesting permission");
+
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_REQUEST_LOCATION:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Thank You For Permission", Toast.LENGTH_SHORT).show();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    Toast.makeText(this, "Can not Continue without Permission", Toast.LENGTH_SHORT).show();
+
+
+                    //make it so nothing can happen till accepted
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+        }
     }
 
     public void setupDagger(){
