b9c4ff8511ddb162cc7867795e8eaaed07e7516b
Richard Rosato <rosatorichard@gmail.com>
2017-08-05 21:27:01 -0400

    more weather info on area

--- app/src/main/java/posidenpalace/com/sirichan/view/activities/weather/Weather.java
+++ app/src/main/java/posidenpalace/com/sirichan/view/activities/weather/Weather.java
@@ -39,6 +39,15 @@ public class Weather extends AppCompatActivity implements WeatherContract.View{
     @BindView(R.id.tvWeatherType)
     TextView weatherType;
 
+    @BindView(R.id.tvWeatherWind)
+    TextView wind;
+
+    @BindView(R.id.tvWeatherHumidity)
+    TextView humidity;
+
+    @BindView(R.id.tvWeatherCity)
+    TextView city;
+
     public void setupDagger(){
         DaggerWeatherComponent.create().inject(this);
     }
@@ -52,11 +61,15 @@ public class Weather extends AppCompatActivity implements WeatherContract.View{
     @Override
     public void weatherResponse(Response<WeatherDataPojo> response) {
 
+        Glide.with(this).load("http://openweathermap.org/img/w/"+response.body().getWeather().get(0).getIcon()+".png").into(weatherPicture);
+        double windSpeed=response.body().getWind().getSpeed()*2.2369;
+        wind.setText("Wind Speed: "+String.format("%.2f", windSpeed)+" Miles/Hour");
         double kelvinTemp=response.body().getMain().getTemp();
         kelvinTemp=(kelvinTemp * 9/5) - 459.67;
         String temp=String.format("%.2f", kelvinTemp);
-        weatherTemperature.setText(temp +"° F");
+        weatherTemperature.setText("Temperature: "+temp +"° F");
         weatherType.setText(response.body().getWeather().get(0).getDescription());
-        Glide.with(this).load("http://openweathermap.org/img/w/"+response.body().getWeather().get(0).getIcon()+".png").into(weatherPicture);
+        humidity.setText("Humidity: "+response.body().getMain().getHumidity()+"%");
+        city.setText("City: "+response.body().getName());
     }
 }
