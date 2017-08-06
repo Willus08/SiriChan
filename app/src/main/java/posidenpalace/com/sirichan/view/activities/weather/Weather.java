cd7cc94a13c6a1ea8b916507895576133328df4e
Richard Rosato <rosatorichard@gmail.com>
2017-08-05 20:50:30 -0400

    glide images from weather

--- app/src/main/java/posidenpalace/com/sirichan/view/activities/weather/Weather.java
+++ app/src/main/java/posidenpalace/com/sirichan/view/activities/weather/Weather.java
@@ -2,15 +2,23 @@ package posidenpalace.com.sirichan.view.activities.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import posidenpalace.com.sirichan.R;
 import posidenpalace.com.sirichan.view.activities.restcalls.model.weathermodel.WeatherDataPojo;
 import posidenpalace.com.sirichan.view.injection.weather.DaggerWeatherComponent;
 import retrofit2.Response;
 
 public class Weather extends AppCompatActivity implements WeatherContract.View{
+    private static final String TAG = "Weather";
     @Inject WeatherPresenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +26,18 @@ public class Weather extends AppCompatActivity implements WeatherContract.View{
         setContentView(R.layout.activity_weather);
         setupDagger();
         presenter.addView(this);
+        ButterKnife.bind(this);
         presenter.getLocationsWeather(33,-87);
     }
+
+    @BindView(R.id.ivWeatherPicture)
+    ImageView weatherPicture;
+
+    @BindView(R.id.tvWeatherTemperature)
+    TextView weatherTemperature;
+
+    @BindView(R.id.tvWeatherType)
+    TextView weatherType;
 
     public void setupDagger(){
         DaggerWeatherComponent.create().inject(this);
@@ -33,5 +51,11 @@ public class Weather extends AppCompatActivity implements WeatherContract.View{
     @Override
     public void weatherResponse(Response<WeatherDataPojo> response) {
 
+        double kelvinTemp=response.body().getMain().getTemp();
+        kelvinTemp=(kelvinTemp * 9/5) - 459.67;
+        String temp=String.format("%.2f", kelvinTemp);
+        weatherTemperature.setText(temp +"° F");
+        weatherType.setText(response.body().getWeather().get(0).getDescription());
+        Glide.with(this).load("http://openweathermap.org/img/w/"+response.body().getWeather().get(0).getIcon()+".png").into(weatherPicture);
     }
 }
