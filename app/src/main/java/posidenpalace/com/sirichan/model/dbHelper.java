e9c70a515de68b9be4d1d49a1843e1bade74db8a
Mikypi3.14159 <Mikypi3.14159@gmail.com>
2017-08-08 18:26:48 -0400

    My work.  List needs to be worked on. 1.2

--- app/src/main/java/posidenpalace/com/sirichan/model/dbHelper.java
+++ app/src/main/java/posidenpalace/com/sirichan/model/dbHelper.java
@@ -6,10 +6,6 @@ package posidenpalace.com.sirichan.model;
 
 import android.app.Application;
 import android.database.sqlite.SQLiteDatabase;
-import android.text.style.TtsSpan;
-
-import java.util.Date;
-import java.util.List;
 
 public class dbHelper extends Application {
 
@@ -25,24 +21,24 @@ public class dbHelper extends Application {
         this.session = daoMaster.newSession();
     }
 
-    public user getUser()
-    {
-        user User = this.session.queryBuilder().where(userDao.Properties.LoggedIn.eq(true));  // should only be one
-        return User;
-    }
+//    public user getUser()
+//    {
+//        //user User= this.session.queryBuilder().where(userDao.Properties.LoggedIn.eq(true));  // should only be one
+//        return User;
+//    }
+//
+//    public List<userEvent> queryEvents()
+//    {
+//        //user User = this.session.queryBuilder().where(userDao.Properties.LoggedIn.eq(true));  // should only be one
+//        return  User.getOrders();
+//    }
 
-    public List<userEvent> queryEvents()
-    {
-        user User = this.session.queryBuilder().where(userDao.Properties.LoggedIn.eq(true));  // should only be one
-        return  User.getOrders();
-    }
-
-    public void logOff()
-    {
-        user User = this.session.queryBuilder().where(userDao.Properties.LoggedIn.eq(true));
-        User.setLoggedIn(false);
-        this.session.update(User);
-    }
+//    public void logOff()
+//    {
+//        //user User = this.session.queryBuilder().where(userDao.Properties.LoggedIn.eq(true));
+//        User.setLoggedIn(false);
+//        this.session.update(User);
+//    }
 
     public void setLogin(String email, String firstName, String lastName)
     {
@@ -54,44 +50,44 @@ public class dbHelper extends Application {
         this.session.insert(User);
     }
 
-    public void insertEvent(userEvent event)
-    {
-        user User = this.getUser();
-        User.getOrders().add(event);
-        this.session.insert(event);
-        this.session.update(User);
-    }
-
-    public void deleteEvent(Date dateTime)
-    {
-        String dateString1 = dateTime.toString();
-        user User = this.getUser();
-        for(userEvent event:User.getOrders())
-        {
-            String dateString2 = event.getDate().toString();
-            if (dateString1.equals(dateString2))
-            {
-                this.session.delete(event);
-                User.getOrders().remove(event);
-                this.session.update(User);
-            }
-        }
-    }
-
-    public Boolean checkConflict(Date dateTime)
-    {
-        Boolean to_return = false; // True if conflict is found
-        String dateString1 = dateTime.toString();
-        user User = this.getUser();
-        for(userEvent event:User.getOrders())
-        {
-            String dateString2 = event.getDate().toString();
-            if (dateString1.equals(dateString2))
-            {
-                to_return = true;
-            }
-        }
-        return to_return;
-    }
+//    public void insertEvent(userEvent event)
+//    {
+//        user User = this.getUser();
+//        User.getOrders().add(event);
+//        this.session.insert(event);
+//        this.session.update(User);
+//    }
+//
+//    public void deleteEvent(Date dateTime)
+//    {
+//        String dateString1 = dateTime.toString();
+//        user User = this.getUser();
+//        for(userEvent event:User.getOrders())
+//        {
+//            String dateString2 = event.getDate().toString();
+//            if (dateString1.equals(dateString2))
+//            {
+//                this.session.delete(event);
+//                User.getOrders().remove(event);
+//                this.session.update(User);
+//            }
+//        }
+//    }
+//
+//    public Boolean checkConflict(Date dateTime)
+//    {
+//        Boolean to_return = false; // True if conflict is found
+//        String dateString1 = dateTime.toString();
+//        user User = this.getUser();
+//        for(userEvent event:User.getOrders())
+//        {
+//            String dateString2 = event.getDate().toString();
+//            if (dateString1.equals(dateString2))
+//            {
+//                to_return = true;
+//            }
+//        }
+//        return to_return;
+//    }
 
 }
