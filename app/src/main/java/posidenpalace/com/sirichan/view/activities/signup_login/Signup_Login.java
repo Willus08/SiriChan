a98e710e4ea3446c3bcc9020b975ecd3b496a94d
Will <baldesw08@gmail.com>
2017-08-16 10:59:35 -0400

    made edit texts have floating hints

--- app/src/main/java/posidenpalace/com/sirichan/view/activities/signup_login/Signup_Login.java
+++ app/src/main/java/posidenpalace/com/sirichan/view/activities/signup_login/Signup_Login.java
@@ -264,13 +264,14 @@ public class Signup_Login extends AppCompatActivity implements Signup_LoginContr
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
                                     if (task.isSuccessful()) {
+                                        Toast.makeText(Signup_Login.this, "Account Created", Toast.LENGTH_SHORT).show();
                                         changeToLogIn();
                                     }
                                     // If sign in fails, display a message to the user. If sign in succeeds
                                     // the auth state listener will be notified and logic to handle the
                                     // signed in user can be handled in the listener.
                                     if (!task.isSuccessful()) {
-                                        Toast.makeText(Signup_Login.this, "failed",
+                                        Toast.makeText(Signup_Login.this, "Account Creation failed",
                                                 Toast.LENGTH_SHORT).show();
                                     }
 
