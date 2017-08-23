cb24b67232f4d65dae3a47273190e4db3d22e1bf
Will <baldesw08@gmail.com>
2017-08-23 11:19:09 -0400

    worki on ui/ux for calander

--- app/src/main/java/posidenpalace/com/sirichan/view/activities/calander/Calander.java
+++ app/src/main/java/posidenpalace/com/sirichan/view/activities/calander/Calander.java
@@ -1,14 +1,11 @@
 package posidenpalace.com.sirichan.view.activities.calander;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
@@ -17,13 +14,13 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import javax.inject.Inject;
 
 import posidenpalace.com.sirichan.R;
 import posidenpalace.com.sirichan.model.RealmDB.RealmHelper;
 import posidenpalace.com.sirichan.view.activities.selected_date.SelectedDate;
-import posidenpalace.com.sirichan.view.activities.setup_event.SetupEvent;
 import posidenpalace.com.sirichan.view.injection.calander.DaggerCalanderComponent;
 
 
@@ -53,6 +50,14 @@ public class Calander extends AppCompatActivity implements CalanderContract.View
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected)
             {
+                if(_Date == date.getDate()){
+                    Intent intent = new Intent(Calander.this, SelectedDate.class);
+                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
+                    intent.putExtra("DATESELECTED", df.format(_Date));
+                    startActivity(intent);
+
+                }
+
                 _Date = date.getDate();
             }
         });
@@ -70,52 +75,52 @@ public class Calander extends AppCompatActivity implements CalanderContract.View
 
 
 
-    // Button CLicks
-    public void onListEvets(View view)
-    {
-        if (_Date != null) {
-            Intent intent = new Intent(Calander.this, SelectedDate.class);
-            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-            intent.putExtra("DATESELECTED", df.format(_Date).toString());
-            startActivity(intent);
-        }else
-        {
-            this.showAlert();
-        }
-    }
-
-    private void showAlert()
-    {
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-        builder1.setMessage("Please Select A Date");
-        builder1.setCancelable(true);
-
-        builder1.setPositiveButton(
-                "Ok",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-
-        AlertDialog alert11 = builder1.create();
-        alert11.show();
-    }
-
-    //Button clicks
-    public void onAddEvent(View view)
-    {
-        if (_Date != null) {
-            Intent intent = new Intent(Calander.this, SetupEvent.class);
-            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-            intent.putExtra("DATESELECTED", df.format(_Date).toString());
-            startActivity(intent);
-        }else
-        {
-           this.showAlert();
-        }
-    }
-
+//    // Button CLicks
+//    public void onListEvets(View view)
+//    {
+//        if (_Date != null) {
+//            Intent intent = new Intent(Calander.this, SelectedDate.class);
+//            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+//            intent.putExtra("DATESELECTED", df.format(_Date).toString());
+//            startActivity(intent);
+//        }else
+//        {
+//            this.showAlert();
+//        }
+//    }
+//
+//    private void showAlert()
+//    {
+//        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+//        builder1.setMessage("Please Select A Date");
+//        builder1.setCancelable(true);
+//
+//        builder1.setPositiveButton(
+//                "Ok",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        dialog.cancel();
+//                    }
+//                });
+//
+//        AlertDialog alert11 = builder1.create();
+//        alert11.show();
+//    }
+//
+//    //Button clicks
+//    public void onAddEvent(View view)
+//    {
+//        if (_Date != null) {
+//            Intent intent = new Intent(Calander.this, SetupEvent.class);
+//            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+//            intent.putExtra("DATESELECTED", df.format(_Date).toString());
+//            startActivity(intent);
+//        }else
+//        {
+//           this.showAlert();
+//        }
+//    }
+//
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
