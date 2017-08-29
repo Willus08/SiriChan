539ba745fe61c2de1eae55d98f5cc3a992fc9db1
Will <baldesw08@gmail.com>
2017-08-29 11:07:52 -0400

    now can go to the chat room need to work on the or statement in the begeining

--- app/src/main/java/posidenpalace/com/sirichan/view/activities/chat/ChatRooms.java
+++ app/src/main/java/posidenpalace/com/sirichan/view/activities/chat/ChatRooms.java
@@ -71,6 +71,22 @@ public class ChatRooms extends AppCompatActivity {
                     listOfChatRooms.clear();
                     listOfChatRooms.addAll(set);
                     arrayAdapter.notifyDataSetChanged();
+                    String voice = getIntent().getStringExtra("voice");
+                    Toast.makeText(ChatRooms.this, voice, Toast.LENGTH_SHORT).show();
+
+                    if(voice != null&& listOfChatRooms.size()>0){
+                        for (int j = 0; j < listOfChatRooms.size() ; j++) {
+                            if (voice.startsWith(listOfChatRooms.get(j).toLowerCase())){
+                                Intent intent = new Intent(ChatRooms.this,FirebasePage.class);
+                                intent.putExtra("chatRoomName", listOfChatRooms.get(j));
+                                intent.putExtra("userName", userName);
+                                intent.putExtra("voice", voice.substring(listOfChatRooms.get(j).length()));
+                                startActivity(intent);
+                                Toast.makeText(ChatRooms.this, "Going to: " + listOfChatRooms.get(j), Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
                 }
             }
 
@@ -80,21 +96,7 @@ public class ChatRooms extends AppCompatActivity {
             }
         });
 
-        String voice = getIntent().getStringExtra("voice");
-        Toast.makeText(this, voice, Toast.LENGTH_SHORT).show();
-        if(voice != null){
-            for (int i = 0; i < listOfChatRooms.size() ; i++) {
-                if (voice.startsWith(listOfChatRooms.get(i))){
-                    Intent intent = new Intent(this,FirebasePage.class);
-                    intent.putExtra("chatRoomName", listOfChatRooms.get(i));
-                    intent.putExtra("userName", userName);
-                    intent.putExtra("voice", voice.substring(listOfChatRooms.get(i).length()));
-                    startActivity(intent);
-                    Toast.makeText(this, "Going to: " + listOfChatRooms.get(i), Toast.LENGTH_SHORT).show();
-                }
 
-            }
-        }
 
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listOfChatRooms);
         chatListView.setAdapter(arrayAdapter);
